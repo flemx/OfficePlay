2e49370d62cbe4431fa625a057d9425629f33141
Damien Fleminks <damien@fleminks.com>
2020-12-24 21:36:52 +0100

    Added player images

--- assets/js/scenes/BootScene.js
+++ assets/js/scenes/BootScene.js
@@ -36,7 +36,7 @@ class BootScene extends Phaser.Scene {
     }
 
     loadPlayer(){
-        this.load.atlas("atlas", "https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/atlas/atlas.png", "https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/atlas/atlas.json");
+        this.load.atlas("atlas", "images/player.png", "images/player.json");
     }
 
     loadSpriteSheets(){
