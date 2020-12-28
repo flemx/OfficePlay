3e5d7de308bea67b0dacc8209c501daf05340209
Damien Fleminks <damien@fleminks.com>
2020-12-28 02:55:01 +0100

    Added binary heap riority queue

--- assets/js/classes/PathFinder.js
+++ assets/js/classes/PathFinder.js
@@ -75,3 +75,102 @@ class PathFinder {
 
 }
 
+
+
+class PriorityQueue{
+
+    
+    constructor(){
+      this.values = [];
+  }
+
+  enqueue(value, priority){
+      let newNode = new Node(value, priority);
+      //console.log('Queue:', newNode);
+      this.values.push(newNode);
+      this.bubbleUp();
+  }
+
+  dequeue(){
+       //remove highest first value and replace with latest
+      const min = this.values[0];
+      const end =  this.values.pop();
+      if(this.values.length > 1){
+           // correct the nodes in the tree with bubbledown method
+          this.values[0] = end;
+          this.bubbleDown();
+      } 
+      return min;
+  }
+  
+  
+  bubbleUp(){
+      // Keep track of index (last inserted item)
+      let idx = this.values.length -1;
+      const element = this.values[idx];
+      //compare to all parents and swap if larger
+       while(idx > 0){
+          //find parent with (n-1)/2 -> floor
+          let parentIdx = Math.floor((idx-1)/2);
+          let parent = this.values[parentIdx];
+          if(element.priority >= parent.priority){
+              //break loop if parent is smaller or equal
+              break;
+           }
+          // swap values if parent is greater
+          this.values[parentIdx] = element;
+          this.values[idx] = parent;
+          idx = parentIdx;
+      }
+
+  }
+
+  bubbleDown(){
+      let idx = 0;
+      const length = this.values.length;
+      const element = this.values[0];
+      while(true){
+          // Get both children based on current parent index 
+          let leftChildIdx = 2 * idx + 1;
+          let rightChildIdx = 2 * idx + 2;
+          let leftChild,rightChild;
+          let swap = null;
+          // Set children nodes if nto out of bounds and keep track of index to swap
+          if(leftChildIdx < length){
+              leftChild = this.values[leftChildIdx];
+              if(leftChild.priority < element.priority){
+                  swap = leftChildIdx;
+              }
+          }
+          if(rightChildIdx < length){
+              rightChild = this.values[rightChildIdx];
+              if(
+                  (swap === null && rightChild.priority < element.priority) ||
+                  (swap !==  null && rightChild.priority < leftChild.priority)
+               ){
+                  swap = rightChildIdx;
+              }
+          }
+          // Check if no swaps were made
+          if(swap === null) break;
+
+          // Swap nodes and update parent index 
+          this.values[idx] = this.values[swap];
+          this.values[swap] = element;
+          idx = swap;
+
+      }
+  }
+
+
+}
+
+
+class Node{
+  constructor(value, priority){
+      this.value = value;
+      this.priority = priority;
+  }
+}
+
+
